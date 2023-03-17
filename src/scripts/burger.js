470832ebde84584246f331f2a471360c701bfcae
”Frederik <fred295q@stud.kea.dk>
2023-03-17 14:36:20 +0100

    header done - mangler styling

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,7 +1,9 @@
 const burgerMenu = document.querySelector("#burger-menu");
 const menu = document.querySelector("#menu");
+const tickets = document.querySelector(".burger-ticket");
 
 burgerMenu.addEventListener("click", function () {
   this.classList.toggle("close");
   menu.classList.toggle("overlay");
+  tickets.classList.toggle("hide");
 });
