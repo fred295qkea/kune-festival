19173bab3b45cec9d20bb534c654505991374f08
Laura Pesando <laur491w@stud.kea.dk>
2023-03-21 14:36:41 +0100

    data

--- src/scripts/lineup.js
+++ src/scripts/lineup.js
@@ -1,4 +1,4 @@
-//https://wzxcaefniwwgggpebhbz.supabase.co
+//https://wzxcaefniwwgggpebhbz.supabase.co/rest/v1/kune
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6eGNhZWZuaXd3Z2dncGViaGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4NzE2ODgsImV4cCI6MTk5NDQ0NzY4OH0.bnekPU1htxCKraVVGX4o2mwA-S58i1jhIBmKNgc7qWc
 
 fetch("https://wzxcaefniwwgggpebhbz.supabase.co/rest/v1/kune", {
